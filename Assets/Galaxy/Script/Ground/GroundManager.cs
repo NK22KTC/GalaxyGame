6ff45ad85809f647e3baeda4701debe6957f667b
Nakaya Shunto <ktc2231020001@edu.kyoto-tech.ac.jp>
2023-12-17 15:21:53 +0900

    プレイヤーが地面に触れるときに発動するギミックを、どのプレイヤーが触れたかにかかわらず初回のみだけ発動するようにした

--- Assets/Galaxy/Script/Ground/GroundManager.cs
+++ Assets/Galaxy/Script/Ground/GroundManager.cs
@@ -1,13 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class GroundManager : MonoBehaviour, IGroundGimmick
 {
-    private bool m_GimmickTriggerd = false;
+    private bool m_GimmickStarted = false;
     private bool m_GimmickCleard = false;
 
-    public bool GimmickTriggerd => m_GimmickTriggerd;
+    public bool GimmickTriggerd => m_GimmickStarted;
     public bool GimmickCleard => m_GimmickCleard;
 
     // Start is called before the first frame update
@@ -24,14 +23,31 @@ public class GroundManager : MonoBehaviour, IGroundGimmick
 
     public void StartButtle()
     {
-        if (m_GimmickTriggerd) return;
+        if (m_GimmickStarted) return;
 
-        Debug.Log("戦闘開始");
-        m_GimmickTriggerd = true;
+        GetComponent<PhotonView>().RPC(nameof(StartGimmick), RpcTarget.AllBuffered);
+        Instantiates();
     }
 
     public void MakeWarpMaker()
     {
 
+    }
+
+    [PunRPC]
+    void StartGimmick()
+    {
+        Debug.Log("戦闘開始");
+        m_GimmickStarted = true;
+    }
+
+    void Instantiates()
+    {
+        PhotonNetwork.Instantiate(GeneralSettings.Instance.m_Prehabs.FlagmentGuide.name, transform.position + new Vector3(0, 3, 0), Quaternion.identity);
+        PhotonNetwork.Instantiate(GeneralSettings.Instance.m_Prehabs.FlagmentLight.name, transform.position + new Vector3(0, 3, 1), Quaternion.identity);
+        PhotonNetwork.Instantiate(GeneralSettings.Instance.m_Prehabs.FlagmentMark.name, transform.position + new Vector3(0, 3, -1), Quaternion.identity);
+        PhotonNetwork.Instantiate(GeneralSettings.Instance.m_Prehabs.FlagmentGuide.name, transform.position + new Vector3(0, 3, 0), Quaternion.identity);
+        PhotonNetwork.Instantiate(GeneralSettings.Instance.m_Prehabs.FlagmentLight.name, transform.position + new Vector3(0, 3, 1), Quaternion.identity);
+        PhotonNetwork.Instantiate(GeneralSettings.Instance.m_Prehabs.FlagmentMark.name, transform.position + new Vector3(0, 3, -1), Quaternion.identity);
     }
 }
